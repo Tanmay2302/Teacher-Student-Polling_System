040b6f96640463590bd8f124e343472cdfe29630
Tanmay2302 <tanmaygurjar2302@gmail.com>
2025-07-19 16:59:09 +0530

    Fix: CORS trailing slash removed for socket.io

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://teacher-student-polling-system.vercel.app/",
+      "https://teacher-student-polling-system.vercel.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,
